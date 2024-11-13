2419e0d59e3514f6c451165051190c8f472ea7af
Ventsislav Kiskinov <venci34@gmail.com>
2024-11-13 12:02:34 +0200

    feature: add home page content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,22 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
-  title: "Booking-app",
-  description: "Clone of booking.com",
+  title: "My bookig app",
+  description: "Booking.com clone app",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Header />
+
         {children}
       </body>
     </html>
