afbdb903dbd9d2995dc6b0b3beca37744e1d99a5
Ventsislav Kiskinov <venci34@gmail.com>
2024-11-14 11:57:01 +0200

    refactor: types rafactor

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import { fetchResults } from "@/lib/fetchResults";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { SearchPageProps } from "@/lib/types";
 
-
-async function SearchPage({ searchParams }: SearchPageProps) {
+type Props = {
+    searchParams: SearchParams;
+  };
+  
+  export type SearchParams = {
+    url: URL;
+    group_adults: string;
+    group_children: string;
+    no_rooms: string;
+    checkin: string;
+    checkout: string;
+  };
+  
+async function SearchPage({ searchParams }: Props ) {
     if (!searchParams.url) return notFound();
   
     const results = await fetchResults(searchParams);
