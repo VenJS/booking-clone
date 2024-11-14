5058f5ab5e58ab9e270673d6bff4036407a2a4b0
Ventsislav Kiskinov <venci34@gmail.com>
2024-11-14 10:21:23 +0200

    feat: oxylabs configuration and fetch results

--- lib/types.ts
+++ lib/types.ts
@@ -10,3 +10,22 @@ export type SearchPageProps = {
     checkin: string;
     checkout: string;
   };
+
+  export type Listing = {
+    url: string;
+    title: string;
+    rating: string | null;
+    description: string;
+    price: string;
+    link: string;
+    booking_metadata: string;
+    rating_word: string;
+    rating_count: string | null;
+  };
+  
+  export type Result = {
+    content: {
+      listings: Listing[];
+      total_listings: string;
+    };
+  };
